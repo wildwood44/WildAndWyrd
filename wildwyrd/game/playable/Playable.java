56acda3fe4e1cbb0bfda47a1e9bc612a13e476fd
wildwood44 <james.stockwell@ntlworld.com>
2023-02-08 20:51:33 +0000

    Add files via upload

---
+++ wildwyrd/game/playable/Playable.java
@@ -0,0 +1,15 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package wildwyrd.game.playable;
+
+import wildwyrd.game.GamePanel;
+import wildwyrd.game.Entity;
+
+public class Playable extends Entity
+{
+    public Playable(final GamePanel gp) {
+        super(gp);
+    }
+}
