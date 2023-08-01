74ffc1e18d7377958e43ec5cc428005b7b747916
wildwood44 <james.stockwell@ntlworld.com>
2023-08-01 15:07:02 +0100

    Larder edit

--- WildAndWyrd/src/wildwyrd/game/object/Obj_Larder.java
+++ WildAndWyrd/src/wildwyrd/game/object/Obj_Larder.java
@@ -22,10 +22,10 @@ public class Obj_Larder extends Entity {
 
 	public void setDialogue() {
 		dialogues[0][0] = new Dialoge(
-				"A small cool cupboard full of shelves and :hooks to store food, mostly empty except :for a loaf of bread.",
+				"A small cool cupboard full of shelves and hooks to store food, mostly empty except for a loaf of bread.",
 				1);
 		dialogues[0][1] = new Dialoge(
-				"Kyla’s familiar Tawie was out resupplying. :Alder was not looking forward to her return.", 1);
+				"Kyla’s familiar Tawie was out resupplying. Alder was not looking forward to her return.", 1);
 		dialogues[1][0] = new Dialoge("There was only a single loaf of bread in the larder. It would have to do.",
 				1);
 		dialogues[2][0] = new Dialoge("The larder was empty.", 1);
